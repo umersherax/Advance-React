41595c268815f751b5421a853e7623f60666cbe4
umersherax <uskdevs@gmail.com>
2022-01-22 16:46:48 +0500

    react hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useRef, useState} from 'react';
 
 function App() {
+  const [name, setName] = useState('')
+  const fff = useRef();
+
+  function aa(){
+    fff.current.focus();
+    console.log(fff)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input ref={fff} value={name} onChange={e => setName(e.target.value)}/>
+      {/* <div ref={prevName}>
+        Name is {name} and to be {prevName.current}
+      </div> */}
+      <button onClick={aa} >
+        Click
+      </button>
     </div>
   );
 }
