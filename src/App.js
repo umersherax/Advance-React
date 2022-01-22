02759d135e3db3660176407b26f55ffb8d9a7955
umersherax <uskdevs@gmail.com>
2022-01-22 17:00:32 +0500

    project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import React, {useEffect, useRef, useState} from 'react';
-
 function App() {
-  const [name, setName] = useState('')
-  const fff = useRef();
-
-  function aa(){
-    fff.current.focus();
-    console.log(fff)
-  }
-
   return (
     <div className="App">
-      <input ref={fff} value={name} onChange={e => setName(e.target.value)}/>
-      {/* <div ref={prevName}>
-        Name is {name} and to be {prevName.current}
-      </div> */}
-      <button onClick={aa} >
-        Click
-      </button>
+      <h1>Master</h1>
     </div>
   );
 }
